3fd8a3e47a459d8dab9e413d941a2fb06e8dded0
shaalx <shaalx@yahoo.com.sg>
2016-06-29 22:08:11 +0800

    assert

--- assert.go
+++ assert.go
@@ -1,12 +1,11 @@
 package assert
-// Testing helpers for doozer.
 
 import (
+	"fmt"
 	"github.com/kr/pretty"
 	"reflect"
-	"testing"
 	"runtime"
-	"fmt"
+	"testing"
 )
 
 func assert(t *testing.T, result bool, f func(), cd int) {
@@ -49,23 +48,50 @@ func Tf(t *testing.T, result bool, format string, args ...interface{}) {
 	tt(t, result, 1, fmt.Sprintf(format, args...))
 }
 
-func Equal(t *testing.T, exp, got interface{}, args ...interface{}) {
-	equal(t, exp, got, 1, args...)
-}
-
-func Equalf(t *testing.T, exp, got interface{}, format string, args ...interface{}) {
-	equal(t, exp, got, 1, fmt.Sprintf(format, args...))
-}
-
-func NotEqual(t *testing.T, exp, got interface{}, args ...interface{}) {
-	fn := func() {
-		t.Errorf("!  Unexpected: <%#v>", exp)
-		if len(args) > 0 {
-			t.Error("!", " -", fmt.Sprint(args...))
+func Equal(t *testing.T, args ...interface{}) {
+	length := len(args)
+	if length <= 1 {
+		return
+	}
+	for i := 0; i < length/2; i += 1 {
+		if length%2 == 1 {
+			equal(t, args[2*i], args[2*i+1], 1, args[length-1])
+		} else {
+			equal(t, args[2*i], args[2*i+1], 1)
 		}
 	}
-	result := !reflect.DeepEqual(exp, got)
-	assert(t, result, fn, 1)
+}
+
+func Equalf(t *testing.T, format string, args ...interface{}) {
+	length := len(args)
+	if length <= 1 {
+		return
+	}
+	for i := 0; i < length/2; i += 1 {
+		if length%2 == 1 {
+			equal(t, args[2*i], args[2*i+1], 1, fmt.Sprintf(format, args[length-1]))
+		} else {
+			equal(t, args[2*i], args[2*i+1], 1)
+		}
+	}
+}
+
+func NotEqual(t *testing.T, args ...interface{}) {
+	length := len(args)
+	if length <= 1 {
+		return
+	}
+	for i := 0; i < length/2; i += 1 {
+		fn := func() {
+			t.Errorf("!  Unexpected: <%#v>", args[2*i])
+			if length%2 == 1 {
+				t.Error("!", " -", fmt.Sprint(args[length-1]))
+			}
+		}
+		result := !reflect.DeepEqual(args[2*i], args[2*i+1])
+		assert(t, result, fn, 1)
+	}
+
 }
 
 func Panic(t *testing.T, err interface{}, fn func()) {
--- assert_test.go
+++ assert_test.go
@@ -5,7 +5,12 @@ import (
 )
 
 func TestLineNumbers(t *testing.T) {
+	Equal(t)
+	Equal(t, "foo")
+	Equal(t, "foo", "foo")
 	Equal(t, "foo", "foo", "msg!")
+	Equal(t, "foo", "foo", "msg!", "msg")
+	Equal(t, "foo", "foo", "msg!", "msg", "haha")
 	//Equal(t, "foo", "bar", "this should blow up")
 }
 
